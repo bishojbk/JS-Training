955cd64d1600315ad7b0b1c8a5c376e0ebcddff1
bishojbk <bishojbk@gmail.com>
2022-05-09 12:23:41 +0545

    Initial Commit

---
+++ Multiplication and Division/index.js
@@ -0,0 +1,8 @@
+let number1 = prompt("Enter a number : ");
+let number2 = prompt("Enter another number :");
+let multiply = number1 * number2;
+console.log(multiply);
+let division = number1 / number2;
+console.log(multiply);
+document.write("The product is : ", multiply + "<br>");
+document.write("The division is : ", division);
