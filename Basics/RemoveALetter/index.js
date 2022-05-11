cb21d13d656e27cb0c56d968f6e3151b5258ebbb
bishojbk <bishojbk@gmail.com>
2022-05-11 13:02:45 +0545

    Initial Commit

---
+++ Basics/RemoveALetter/index.js
@@ -0,0 +1,11 @@
+function Remove() {
+  let inputS = document.getElementById("string").value;
+  let inputI = document.getElementById("int").value;
+  inputI = parseInt(inputI) - 1;
+  let letter = inputS.charAt(inputI);
+  console.log(letter);
+  letter = letter.toString();
+  let newString = inputS.replace("letter", "");
+  console.log(newString);
+  //   document.write("The result after removing the letter is : ", newString);
+}
