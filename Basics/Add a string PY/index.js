4360cded95a6ca1ee60481758ee5360c9cf09801
EJR Bisoj <51935743+bishojbk@users.noreply.github.com>
2022-05-11 22:05:47 +0545

    Corrected

--- Basics/Add a string PY/index.js
+++ Basics/Add a string PY/index.js
@@ -1,12 +1,12 @@
 function Add() {
-  let input = document.getElementById("string").value;
+  const input = document.getElementById("string").value;
   console.log(input);
-  let s1 = input.charAt(0);
+  let letter1 = input.charAt(0);
   console.log(s1);
-  let s2 = input.charAt(1);
-  let s3 = s1 + s2;
-  s3 = s3.toLowerCase();
-  if (s3 == "py") {
+  let letter2 = input.charAt(1);
+  let total = letter1 + letter2;
+  total = total.toLowerCase();
+  if (total == "py") {
     document.write("The string is : ", input);
   } else {
     document.write("The string is : ", "Py" + input);
