46c0b3c93b2f6e12f41dba9a1a90f2362d08798b
bishojbk <bishojbk@gmail.com>
2022-05-13 07:15:03 +0545

    Solve issues

--- Basics/Largest of 3 Numbers/index.js
+++ Basics/Largest of 3 Numbers/index.js
@@ -1,8 +1,11 @@
 function Greatest() {
-  const number1 = document.getElementById("int1").value;
-  const number2 = document.getElementById("int2").value;
-  const number3 = document.getElementById("int3").value;
-  console.log(number1);
+  let number1 = document.getElementById("int1").value;
+  let number2 = document.getElementById("int2").value;
+  let number3 = document.getElementById("int3").value;
+  number1 = parseInt(number1);
+  number2 = parseInt(number2);
+  number3 = parseInt(number3);
+  console.log(number1, number2, number3);
   if (number1 > number2 && number1 > number3) {
     document.write(number1, " is the greatest.");
   } else if (number2 > number1 && number2 > number3) {
