e0bba148fc6f5a4dfd4fe198860f90ee2a105ed1
bishojbk <bishojbk@gmail.com>
2022-05-18 17:17:52 +0545

    Updated

--- Basics/Number Multiple of 7 or 11/index.js
+++ Basics/Number Multiple of 7 or 11/index.js
@@ -1,20 +1,17 @@
 function Multiple() {
   let number1 = document.getElementById("int1").value;
   let number2 = document.getElementById("int2").value;
-  if (number1 < 0 || number2 < 0) {
-    document.write("Please, enter a positive number. ");
-  } else {
-    if (number1 % 7 == 0 && number2 % 7 !== 0) {
-      document.write("number1 is a multiple of 7.");
-    } else if (number1 % 11 == 0 && number2 % 11 !== 0) {
-      document.write("number1 is a multiple of 11. ");
-    } else {
-      if (number2 % 7 == 0 && number1 % 7 !== 0) {
-        document.write("number2, is a multiple of 7.");
-        if (number2 % 11 == 0 && number1 % 11 !== 0) {
-          document.write("number2 is a multiple of 11.");
-        }
-      }
-    }
-  }
+  if (
+    !(
+      (number1 % 7 == 0 || number1 % 11 == 0) &&
+      (number2 % 7 == 0 || number2 % 11 == 0)
+    )
+  ) {
+    document.write(
+      number1 % 7 == 0 ||
+        number1 % 11 == 0 ||
+        number2 % 7 == 0 ||
+        number2 % 11 == 0
+    );
+  } else document.write("False");
 }
