8c8367dc824cfde023691d03433adaccf1048786
bishojbk <bishojbk@gmail.com>
2022-05-18 15:58:01 +0545

    Replace a character by its following

---
+++ Basics/Replace A Character/index.js
@@ -0,0 +1,12 @@
+function Replace() {
+  let string = document.getElementById("string").value;
+  console.log(string);
+  let length = string.length;
+  let array = string.split("");
+  for (let x = 0; x < length; x++) {
+    let ascii = array[x].charCodeAt() - "a".charCodeAt();
+    ascii = (ascii + 1) % 26;
+    array[x] = String.fromCharCode(ascii + "a".charCodeAt());
+  }
+  console.log(array.join(""));
+}
