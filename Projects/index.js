3a0de957b7920a2fd0020360a0bd8ea311e679ac
bishojbk <bishojbk@gmail.com>
2022-05-26 21:20:41 +0545

    Counter

---
+++ Projects/index.js
@@ -0,0 +1,13 @@
+let clicks = 0;
+
+function ICounter() {
+  document.getElementById("clicks").innerHTML = clicks;
+
+  clicks += 1;
+}
+function DCounter() {
+  if (clicks >= 0) {
+    document.getElementById("clicks").innerHTML = clicks;
+    clicks -= 1;
+  }
+}
