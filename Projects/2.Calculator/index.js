3754630ff13a10b33c536eb1e7167bd88995c318
bishojbk <bishojbk@gmail.com>
2022-05-30 22:05:32 +0545

    Updated

--- Projects/2.Calculator/index.js
+++ Projects/2.Calculator/index.js
@@ -1,9 +1,31 @@
-let clicks = 0;
+let clicks = "";
 function clearScreen() {
-  document.getElementById("clicks").innerHTML = clicks;
   clicks = 0;
+  document.getElementById("clicks").innerHTML = clicks;
+  clicks = "";
 }
 function updateScreen(a) {
-  console.log(a);
-  clicks = a;
+  if (clicks == "." && a == ".") {
+    return;
+  } else {
+    clicks = clicks + a;
+    document.getElementById("clicks").innerHTML = clicks;
+  }
 }
+
+function displayScreen() {
+  let val = clicks;
+  console.log(val);
+  val = eval(val);
+  clicks = val;
+  document.getElementById("clicks").innerHTML = clicks;
+}
+// function deleteScreen() {
+//   let del = clicks.split("");
+//   console.log(del);
+//   let i = del.length;
+//   for (let j = 0; j <= i; j++) {
+//     clicks = clicks + "";
+//   }
+//   document.getElementById("clicks").innerHTML = clicks;
+// }
