860fed73146d070a7ee35902ebeb7bdee0cd6cf9
bishojbk <bishojbk@gmail.com>
2022-05-30 08:05:24 +0545

    Updated

---
+++ JPT/index.js
@@ -0,0 +1,17 @@
+screen = document.getElementById("screen-content");
+
+function updatescreen(e) {
+  document.getElementById("blinker").style.visibility = "hidden";
+  screen.innerHTML = screen.textContent + e;
+}
+
+function clearscreen() {
+  screen.innerHTML = "";
+  document.getElementById("blinker").style.visibility = "hidden";
+}
+
+function calculateResult() {
+  let val = screen.textContent;
+  val = eval(val);
+  screen.innerHTML = val;
+}
