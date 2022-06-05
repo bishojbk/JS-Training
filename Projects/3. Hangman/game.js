6ae911dcedf5899e5a811a297be101784bf01b78
bishojbk <bishojbk@gmail.com>
2022-06-05 07:22:56 +0545

    Resolve PR Comments

--- Projects/3. Hangman/game.js
+++ Projects/3. Hangman/game.js
@@ -1,33 +1,47 @@
+let array = ["banana", "avocado", "apple", "mango", "kiwi", "melon", "litchi"];
+let i = Math.floor(Math.random() * 7 + 0);
+
+let theWord = array[i];
+
+let length = theWord.length;
+
+let j = Math.floor(Math.random() * length + 0);
+
+let k = Math.floor(Math.random() * length + 0);
+
+let m = Math.floor(Math.random() * length + 0);
+
+let text = theWord.split("");
+
+let text1 = text.toString();
+
+let c1 = text[j];
+let c2 = text[k];
+let c3 = text[m];
+
 function Start() {
-  let array = ["banana", "avocado", "apple", "mango", "kiwi"];
-  let i = Math.floor(Math.random() * 5 + 0);
-  console.log(i);
-  let theWord = array[i];
-  console.log(theWord);
-  let length = theWord.length;
-  let j = Math.floor(Math.random() * length + 0);
-  console.log(j);
-  let k = Math.floor(Math.random() * length + 0);
-  console.log(k);
-  let text = theWord.split("");
-  let m = Math.floor(Math.random() * length + 0);
-  let c1 = text[j];
-  let c2 = text[k];
-  let c3 = text[m];
+  document.getElementById("start").innerHTML = "";
   text[j] = "_";
   text[k] = "_";
   text[m] = "_";
+
   text = text.join(" ");
-  console.log(text);
   document.getElementById("words").innerHTML = text;
 }
 
-// let tax = text.split(" ");
-//   let fax = text.split(" ");
-//   console.log(tax);
-//   let taxLength = tax.length;
-//   for (let b = 0; b <= taxLength; b++) {
-//     if (tax[b] == "_") {
-//       fax[b] = e;
-//     }
-//     fax = fax.join(" ");
+function words(e) {
+  {
+    let tax = text.split(" ");
+    let fax = text.split(" ");
+    console.log(tax);
+    let taxLength = tax.length;
+    for (let b = 0; b <= taxLength; b++) {
+      if (tax[b] == "_") {
+        if (e == c1 || e == c2 || e == c3) fax[b] = e;
+        console.log(fax[b]);
+      }
+      let notFax = fax.join(" ");
+      document.getElementById("words").innerHTML = notFax;
+    }
+  }
+}
